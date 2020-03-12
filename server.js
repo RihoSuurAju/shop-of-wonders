2112b559ff314ca272c97122837fb0d422e0f08a
RihoSuurAju <rihomartin.haavasalu@gmail.com>
2020-03-12 22:15:05 +0200

    another oopsie

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ app.use(function(req, res, next){
 
 
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is vibing on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
 });
-
-
